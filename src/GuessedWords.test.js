a00f87917673c232f749ef4f53053360d8d6cd86
ramin <rx.azhdari@gmail.com>
2020-01-16 12:04:10 +0100

    Add tests for GuessedWords component

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -39,5 +39,27 @@ describe('if there are no words guessed', () => {
   })
 });
 describe('if there are words guessed', () => {
+  let wrapper;
+  const guessedWords = [
+    { guessedWord: 'train', letterMatchCount: 3 },
+    { guessedWord: 'agile', letterMatchCount: 1 },
+    { guessedWord: 'party', letterMatchCount: 5 },
+  ]
 
+  beforeEach(() => {
+    wrapper = setup({ guessedWords });
+  })
+
+  test('renders without error', () => {
+    const component = findByTestAttribute(wrapper, 'component-guessed-words');
+    expect(component.length).toBe(1);
+  });
+  test('renders guessed word section', () => {
+    const guessedWordsNode = findByTestAttribute(wrapper, 'guessed-words');
+    expect(guessedWordsNode.length).toBe(1)
+  });
+  test('correct number of guessed words', () => {
+    const guessedWordsNodes = findByTestAttribute(wrapper, 'guessed-word');
+    expect(guessedWordsNodes.length).toBe(guessedWords.length);
+  });
 })
