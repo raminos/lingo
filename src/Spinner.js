28726c609716a5ea3c3c84a35ed0da77508ff41b
ramin <rx.azhdari@gmail.com>
2020-01-18 21:27:16 +0100

    Refactor Spinner component and create additional tests

--- src/Spinner.js
+++ src/Spinner.js
@@ -1,28 +1,36 @@
 import React from 'react';
 
-const Spinner = () => (
-  <div
-    className="container"
-    data-test="spinner"
-  >
-    <div className="d-flex justify-content-center">
-      <div className="row">
-        <div className="">
-          <div
-            className="spinner-border mx-5 text-muted"
-            role="status"
-          >
-            <span className="sr-only">
-              Loading...
+import languageContext from './contexts/languageContext';
+import stringModule from './helpers/strings';
+
+const Spinner = () => {
+  const [language] = languageContext.useLanguage();
+  return (
+    <div
+      className="container"
+      data-test="component-spinner"
+    >
+      <div className="d-flex justify-content-center">
+        <div className="row">
+          <div className="">
+            <div
+              className="spinner-border mx-5 text-muted"
+              role="status"
+            >
+              <span className="sr-only">
+                Loading...
             </span>
-          </div>
-          <div className="row">
-            <p className="text-muted">Loading secret word...</p>
+            </div>
+            <div className="row">
+              <p className="text-muted">
+                {stringModule.getStringByLanguage(language, 'loading')}
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Spinner;
---
+++ src/Spinner.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { findByTestAttribute } from '../test/testUtilities';
+
+import Spinner from './Spinner';
+import languageContext from './contexts/languageContext';
+
+const setup = (language = 'en') => {
+  const wrapper = mount(
+    <languageContext.LanguageProvider value={[language, jest.fn()]}>
+      <Spinner />
+    </languageContext.LanguageProvider>
+  );
+  const component = findByTestAttribute(wrapper, 'component-spinner');
+  return component;
+}
+
+test('renders without errors', () => {
+  const spinnerComponent = setup();
+  expect(spinnerComponent.exists()).toBe(true);
+});
+test('displays expected text in French', () => {
+  const spinnerComponent = setup('fr');
+  expect(spinnerComponent.text()).toMatch(/chargement/i);
+});
